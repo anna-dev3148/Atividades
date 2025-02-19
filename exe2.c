cd9eaf851477c8d621a1b4d1ad33d332930cf376
Anna Letícia Silva <annaleticiabarbosadasilva@gmail.com>
2025-02-18 22:20:10 -0300

    Add files via upload

---
+++ exe2.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main() {
+  int ano = 2007;
+  int idade = 2026 - ano;
+  printf("ano: %d\n", ano);
+  printf("idade: %d", idade);
+  return 0;
+} 
---
+++ exe4.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+int main() {
+  char letra = 'Y';
+  char proxima = letra + 1;
+  printf("Letra: %c\n", proxima);
+  proxima = letra + 2;
+  printf("Letra: %c\n", proxima);
+  proxima = letra + 3;
+  printf("Letra: %c\n", proxima);
+  proxima = letra + 4;
+  printf("Letra: %c\n", proxima);
+  proxima = letra + 5;
+  printf("Letra: %c\n", proxima);
+  proxima = letra + 6;
+  printf("Letra: %c\n", proxima);
+  proxima = letra + 7;
+  printf("Letra: %c\n", proxima);
+  proxima = letra + 8;
+  printf("Letra: %c\n", proxima);
+  proxima = letra + 9;
+  printf("Letra: %c\n", proxima);
+  proxima = letra + 10;
+  printf("Letra: %c\n", proxima);
+  return 0;
+}  
